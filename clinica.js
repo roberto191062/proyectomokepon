a92a4f78b1f378d37f9e361f5aa5a876f0aaae5d
roberto191062 <127068930+roberto191062@users.noreply.github.com>
2023-10-30 00:01:15 -0300

    Add files via upload

--- clinica.js
+++ clinica.js
@@ -4,8 +4,8 @@
 
 class Documento {
 
-    constructor(numerodocumento, apellido, nombre, edad, sexo, direccion, obrasocial) {
-        this.numerodocumento = numerodocumento
+    constructor(dni, apellido, nombre, edad, sexo, direccion, obrasocial) {
+        this.dni = dni
         this.apellido = apellido
         this.nombre = nombre
         this.edad = edad
@@ -19,44 +19,48 @@ class Documento {
 // DOM Events
 //document.getElementById('ingreso')
 
-    //.addEventListener('submit', function (e) {
-     function captura() {   
-        const apellido = document.getElementById('apellido').value;
-         
-        const nombre = document.getElementById('nombre').value;
+//.addEventListener('submit', function (e) {
+function captura() {
+    const dni = document.getElementById('dni').value;
+    const apellido = document.getElementById('apellido').value;
 
-        const edad = document.
+    const nombre = document.getElementById('nombre').value;
+
+    const edad = document.
         getElementById('edad').value;
 
-        const sexo = document.getElementById('sexo').value;
+    const sexo = document.getElementById('sexo').value;
 
-        const direccion = document.getElementById('direccion').value;
+    const direccion = document.getElementById('direccion').value;
 
-        const obrasocial = document.getElementById('obrasocial').value;
+    const obrasocial = document.getElementById('obrasocial').value;
 
-        console.log(apellido,nombre,edad,sexo)
+   // console.log(dni,apellido, nombre, edad, sexo)
+}
+
+
+const Docu = new Documento(dni, apellido, nombre, sexo, edad, direccion, obrasocial)
+
+console.log(Docu(dni,apellido, ))
+captura(dni, apellido, nombre, sexo, direccion, obrasocial)
+{
+    if (dni == Docu(dni)) {
+        alert("cargar turno")
     }
-        
+    else { 
+    // e.preventDefault();
+    alert("dar turno")}
 
-        const Docu = new Documento(apellido, nombre, sexo, edad, direccion, obrasocial)
-
-        console.log( Docu(apellido,nombre,edad,sexo,obrasocial))
-
-        const UIT = new UI();
-        UIT.addproduct(Docu);
-
-        e.preventDefault();
+}
 
 
 
-
-    
 
 class UI {
     addDocumentos(Docu) {
         const listado = document.getElementById('listado');
         const element = document.createElement('div');
-        element.innerHTML =`
+        element.innerHTML = `
             <div class ="tarjeta">
                 <div class="tarjeta formato">
                     <strong> Documentos Apellido</strong> : ${Docu.apellido}
@@ -70,7 +74,14 @@ class UI {
 
             </div> `;
 
-            listado.appendChild(element);
+        listado.appendChild(element);
+
+        const UIT = new UI();
+        UIT.addproduct(Docu);
+
+
+
+
 
     }
 
